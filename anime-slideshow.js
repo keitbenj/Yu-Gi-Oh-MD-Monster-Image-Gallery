b6adf2d2a5797e7057135d58e48db9de6a9afbb0
Ben Keith <47278981+keitbenj@users.noreply.github.com>
2025-12-08 17:03:56 -0500

    Add files via upload
    Completely changed the slideshow mechanics to now use anime.js

---
+++ anime-slideshow.js
@@ -0,0 +1,54 @@
+
+window.onload = function(){
+    document.querySelectorAll('.img-container').forEach(container => new AnimeSlideshow(container));
+}
+
+class AnimeSlideshow{
+    constructor(container){
+        this.container = container;
+        this.current = 0;
+        this.slides = container.querySelectorAll('.slide');
+
+        this.prevBtn = container.querySelector(".prevBtn");
+        this.nextBtn = container.querySelector(".nextBtn");
+
+        this.prevBtn.addEventListener("click", () => this.showSlide(this.current - 1));
+        this.nextBtn.addEventListener("click", () => this.showSlide(this.current + 1));
+    }
+    
+
+    showSlide(index) {
+        const nextIndex = (index + this.slides.length) % this.slides.length;
+        if (nextIndex === this.current) return;
+
+        const currentSlide = this.slides[this.current];
+        const nextSlide = this.slides[nextIndex];
+
+        // Animate current slide out
+        anime({
+            targets: currentSlide,
+            translateX: [-0, -200],
+            opacity: [1, 0],
+            duration: 500,
+            easing: 'easeOutQuad',
+            complete: () => {
+            currentSlide.classList.remove('active');
+            }
+        });
+
+        // Animate next slide in
+        nextSlide.classList.add('active');
+        anime({
+            targets: nextSlide,
+            translateX: [200, 0],
+            opacity: [0, 1],
+            duration: 500,
+            easing: 'easeOutQuad'
+        });
+
+        this.current = nextIndex;
+    }
+}
+
+
+
